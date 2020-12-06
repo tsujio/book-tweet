3a15a4091625c2bc56bd72e92520eb3d5787c34c
tsujio <ntsujio@gmail.com>
2020-12-06 22:12:27 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 cd "$SCRIPT_DIR" && \
     npm run build && \
-    rm -rf docs && \
-    cp -r build docs
-
-echo "Build succeeded."
+    find docs/* -maxdepth 0 | grep -v 'CNAME' | xargs rm -rf && \
+    cp -r build/* docs && \
+    echo "Build succeeded."
