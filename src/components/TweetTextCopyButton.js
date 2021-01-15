328aa073c8acc07460b6eb0e29f10e44b254ec9d
tsujio <ntsujio@gmail.com>
2021-01-15 23:53:11 +0900

    add tooltip

--- src/components/TweetTextCopyButton.js
+++ src/components/TweetTextCopyButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { CardText } from 'react-bootstrap-icons';
 import { getTweetText } from '../lib/tweet';
 import './TweetTextCopyButton.css';
@@ -7,13 +7,24 @@ import './TweetTextCopyButton.css';
 export default class TweetTextCopyButton extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      tooltipShown: false
+    }
     this.handleClick = this.handleClick.bind(this)
+    this.handleTooltipToggle = this.handleTooltipToggle.bind(this)
   }
 
   handleClick(event) {
     if (navigator.clipboard) {
       navigator.clipboard.writeText(getTweetText(this.props.book))
     }
+  }
+
+  handleTooltipToggle(show) {
+    this.setState({tooltipShown: show})
+    setTimeout(() => {
+      this.setState({tooltipShown: false})
+    }, 1000)
   }
 
   render() {
@@ -24,9 +35,17 @@ export default class TweetTextCopyButton extends React.Component {
     return (
       <Row>
         <Col>
-          <Button className="tweet-text-copy-button"
-                  variant="light"
-                  onClick={this.handleClick}><CardText /></Button>
+          <OverlayTrigger placement="bottom"
+                          trigger="click"
+                          show={this.state.tooltipShown}
+                          onToggle={this.handleTooltipToggle}
+                          overlay={(props) => (
+                            <Tooltip {...props}>Copied</Tooltip>
+                          )}>
+            <Button className="tweet-text-copy-button"
+                    variant="light"
+                    onClick={this.handleClick}><CardText /></Button>
+          </OverlayTrigger>
         </Col>
       </Row>
     )
